072f9faada7bac72ceff098d1976fe104ad38996
Daniel Barczyński <daniel.barczynski@gearcode.eu>
2023-04-17 23:32:45 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learning-angular-2';
+  style: string = 'color: red; font-size: 20px;'
+  imgSrc: string = 'https://angular.io/assets/images/logos/angular/angular.png';
+  car: string = "Opel Astra H";
+  cat: string = "";
+  click(): void {
+    this.car = "";
+  }
+  log(animal: string | null): void {
+    console.log(animal);
+  }
 }
