083c0e4c2b093159fd8bb21356de82d6a2049a0e
Daniel Barczyński <daniel.barczynski@onet.pl>
2023-04-18 22:07:13 +0200

    ngIf & ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
 
+interface Cat {
+  name: string;
+  age: number;
+  color: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  cats: Cat[] = [
+    { name: 'Tom', age: 3, color: 'white' },
+    { name: 'Jerry', age: 2, color: 'brown' },
+    { name: 'Mickey', age: 1, color: 'black' },
+  ];
+
   title = 'learning-angular-2';
   style: string = 'color: red; font-size: 20px;'
   imgSrc: string = 'https://angular.io/assets/images/logos/angular/angular.png';
-  car: string = "Opel Astra H";
+  car: string = "BMW";
+  isTrue: boolean = false;
   cat: string = "";
   click(): void {
     this.car = "";
