aba8b4b109ead70836e3df49302f903e5f98e756
Daniel Barczyński <daniel.barczynski@gearcode.eu>
2023-04-23 11:22:17 +0200

    constructor & ngOnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
-interface Cat {
+interface Pokemon {
   name: string;
   age: number;
   color: string;
-  isMy: boolean;
 }
 
 @Component({
@@ -14,23 +13,24 @@ interface Cat {
 })
 
 export class AppComponent {
-  cats: Cat[] = [
-    { name: 'Tom', age: 3, color: 'white', isMy: true },
-    { name: 'Jerry', age: 2, color: 'brown', isMy: false },
-    { name: 'Mickey', age: 1, color: 'black', isMy: true },
+  pokemons: Pokemon[] = [
+    { name: 'Levi', age: 3, color: 'white'},
+    { name: 'Nano', age: 5, color: 'brown',},
+    { name: 'Zirus', age: 4, color: 'black'},
   ];
-  fontsize: number = 25;
-  color: string = 'pink';
-  title = 'learning-angular-2';
-  style: string = 'color: red; font-size: 20px;'
-  imgSrc: string = 'https://angular.io/assets/images/logos/angular/angular.png';
-  car: string = "BMW";
-  isTrue: boolean = false;
-  cat: string = "";
-  click(): void {
-    this.car = "";
-  }
-  log(animal: string | null): void {
-    console.log(animal);
-  }
+  
+  // fontsize: number = 25;
+  // color: string = 'pink';
+  // title = 'learning-angular-2';
+  // style: string = 'color: red; font-size: 20px;'
+  // imgSrc: string = 'https://angular.io/assets/images/logos/angular/angular.png';
+  // car: string = "BMW";
+  // isTrue: boolean = false;
+  // cat: string = "";
+  // click(): void {
+  //   this.car = "";
+  // }
+  // log(animal: string | null): void {
+  //   console.log(animal);
+  // }
 }
--- src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.css']
 })
-export class PokemonListComponent {
-
+export class PokemonListComponent implements OnInit{
+  pokemon: string = '';
+  constructor() {
+    this.pokemon = 'Pikachu';
+    console.log('con');
+   }
+   
+  ngOnInit(): void {
+    console.log(this.pokemon);
+    console.log('PokemonListComponent initialized');
+  }
 }
