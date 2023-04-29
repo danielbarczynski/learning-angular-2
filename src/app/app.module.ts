a9c7aaf1044fdf05bbb0888baa0c2fb5fac4dd8b
Daniel Barczyński <daniel.barczynski@gearcode.eu>
2023-04-29 15:06:19 +0200

    home and about routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,26 @@ import { AppComponent } from './app.component';
 import { PokemonBaseModule } from './pokemon-base/pokemon-base.module';
 import { PokemonTemplateFormComponent } from './pokemon-template-form/pokemon-template-form.component';
 import { PokemonPipe } from './pipes/pokemon.pipe';
+import { AboutComponent } from './about/about.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [				
     AppComponent,
       PokemonTemplateFormComponent,
-      PokemonPipe
+      PokemonPipe,
+      AboutComponent,
+      HomeComponent,
+      NotfoundComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    PokemonBaseModule
+    PokemonBaseModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
